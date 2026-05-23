0b6118f4c01500b70c36aaae72b08c5e21558df2
YanikBo <yanik@boehi.name>
2026-05-23 16:07:37 +0200

    feat: last touches

--- Projekt_Portfolio/src/app/Services/project.servive.ts
+++ Projekt_Portfolio/src/app/Services/project.servive.ts
@@ -14,6 +14,7 @@ export class ProjectServive {
         'Entwicklung eines Finanztrackers mittels Java Swing zur effizienten Budgetplanung sowie zur Erfassung von Einnahmen und Ausgaben. Man kann selbstständig Kategorien erstellen. ',
         'Der Finanztracker war mein erstes eigenes Projekt und hat mit geholfen Java und das Objektorientierte Programmieren besser zu verstehen und anzuwenden. Es ist aus der Idee entstanden, dass ich ein Tool zur Verwaltung meiner Finanzen benötige. Das Projekt umfasst Funktionen wie die Kategorisierung von Transaktionen, Budgets festzulegen. Er ist noch in arbeite, welche allerdings zurzeit pausiert ist.',
         '/tracker.png',
+        '',
         ''
       )
     );
@@ -21,9 +22,10 @@ export class ProjectServive {
       new Project(
         'Racing-Game',
         'Mit Java Swing entwickeltes Rennspiel, das sowohl einen Einzel- als auch einen Mehrspielermodus bietet. Es gibt einen Strecken-Editor sowie die Möglichkeit zur Fahrzeugkonfiguration.',
-        'Dieses Rennspiel entstand ebenfalls als Übung, ich durfte im Betrieb ein Projekt umsetzten um Java besser zu verstehen. Ich habe mich für ein einfaches Rennspiel entschieden wegen meiner fasznation für Autos. Es bietet die Möglichkeit, zwischen einem Einzel- und Mehrspielermodus zu wechseln, wobei der Mehrspielermodus über das Netzwerk realisiert ist. Zusätzlich verfügt das Spiel über einen Strecken-Editor, mit dem Spieler ihre eigenen Rennstrecken erstellen können, sowie eine Fahrzeugkonfiguration, die es ermöglicht, verschiedene Aspekte der Fahrzeuge anzupassen.',
+        'Dieses Rennspiel entstand ebenfalls als Übung, ich durfte im Betrieb ein Projekt umsetzten um Java besser zu verstehen. Ich habe mich für ein einfaches Rennspiel entschieden wegen meiner fasznation für Autos. Es bietet die Möglichkeit, zwischen einem Einzel- und Mehrspielermodus zu wechseln. Zusätzlich verfügt das Spiel über einen Strecken-Editor, mit dem Spieler ihre eigenen Rennstrecken erstellen können, sowie eine Fahrzeugkonfiguration, die es ermöglicht, verschiedene Aspekte der Fahrzeuge anzupassen.',
         '/race.png',
-        'h'
+        '/projects/Racegame.mp4',
+        '/projects/Racegame.zip'
       )
     );
     this.projects.push(
@@ -32,7 +34,8 @@ export class ProjectServive {
         'Implementierung des Klassikers Tetris auf einem ESP32-Mikrocontroller (Arduino) und mit C programmiert. Das Projekt umfasst ein Leaderboard-System sowie anpassbare Farbeinstellungen.',
         'Das war mein erstes grösseres Programmierprojekt überhaupt welches ich praktisch komplett alleine umgesetzt habe. Es war eine grosse Herausforderung, da ich zu diesem Zeitpunkt noch nicht viel Erfahrung mit der Programmierung hatte. Das Projekt beinhaltet die Implementierung des klassischen Spiels Tetris auf einem ESP32-Mikrocontroller unter Verwendung der Arduino-Plattform. Es bietet ein Leaderboard-System, das es den Spielern ermöglicht, ihre Punktzahlen zu speichern und mit anderen zu vergleichen. Zudem gibt es anpassbare Farbeinstellungen, die es den Spielern erlauben, das Erscheinungsbild des Spiels nach ihren Vorlieben zu gestalten.',
         '/tetris.png',
-        'h'
+        '',
+        '/projects/tetris.zip'
       )
     );
   }
--- Projekt_Portfolio/src/app/classes/project.ts
+++ Projekt_Portfolio/src/app/classes/project.ts
@@ -2,14 +2,16 @@ export class Project {
     public name: string;
     public description: string;
     public image: string;
+    public video: string;
     public file: string;
     public detaildescription: string;
 
-    constructor(name: string, description: string, detaildescription: string, image: string, file: string) {
+    constructor(name: string, description: string, detaildescription: string, image: string, video: string, file: string) {
         this.name = name;
         this.description = description;
         this.detaildescription = detaildescription;
         this.image = image;
+        this.video = video;
         this.file = file;
     }
 }
