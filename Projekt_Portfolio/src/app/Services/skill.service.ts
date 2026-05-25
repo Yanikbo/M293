0b0a550d2648f9e1a96d4198a3e8541e418661ee
YanikBo <yanik@boehi.name>
2026-05-25 11:28:11 +0200

    fix: images pages

--- Projekt_Portfolio/src/app/Services/skill.service.ts
+++ Projekt_Portfolio/src/app/Services/skill.service.ts
@@ -12,7 +12,7 @@ export class SkillService {
       new Skill(
         'JAVA',
         'Durch das Studium von Fachliteratur und die Umsetzung verschiedener Projekte, wie etwa eines Finanztrackers oder eines Rennspiels, konnte ich fundierte Kenntnisse in Java erwerben.',
-        '/java.png',
+        'java.png',
         'Average'
       )
     );
@@ -20,7 +20,7 @@ export class SkillService {
       new Skill(
         'C',
         'C war meine erste Programmiersprache, in der ich sowohl schulische Module als auch praktische Projekte im Bereich der Arduino-Programmierung erfolgreich abgeschlossen habe.',
-        '/c.png',
+        'c.png',
         'Average'
       )
     );
@@ -28,7 +28,7 @@ export class SkillService {
       new Skill(
         'Angular',
         'Das Angular-Framework setze ich primär im beruflichen Umfeld ein, wo ich bereits diverse Übungen und praxisnahe Projekte realisiert habe.',
-        '/angular.png',
+        'angular.png',
         'Beginner'
       )
     );
@@ -36,7 +36,7 @@ export class SkillService {
       new Skill(
         'SQL',
         'Aktuell vertiefe ich meine SQL-Kenntnisse im schulischen Rahmen und habe einen überbetrieblichen Kurs (ÜK) abgeschlossen. Beruflich sammle ich zudem Erfahrung in der Arbeit mit JPQL.',
-        '/sql.png',
+        'sql.png',
         'Average'
     )
     );
@@ -44,7 +44,7 @@ export class SkillService {
       new Skill(
         'Drawing',
         'Neben der Programmierung widme ich mich seit vielen Jahren dem Zeichnen. Ein besonderer Meilenstein war meine künstlerische Abschlussarbeit in der Sekundarschule.',
-        '/drawing.png',
+        'drawing.png',
         'Advanced'
       )
     );
@@ -52,7 +52,7 @@ export class SkillService {
       new Skill(
         'Swimming',
         'Seit meiner Kindheit ist das Schwimmen ein fester Bestandteil meines Alltags. Nach langjähriger Vereinszugehörigkeit im Schwimmclub trainiere ich heute wöchentlich im privaten Rahmen.',
-        '/swimming.png',
+        'swimming.png',
         'Advanced'
       )
     );
