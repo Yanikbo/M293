bb3b21c912ccea08f1205847125258ef11011622
YanikBo <yanik@boehi.name>
2026-06-01 11:21:52 +0200

    fix: delete detailview

--- Projekt_Portfolio/src/app/project-detailview/project-detailview.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-project-detailview',
-  imports: [],
-  templateUrl: './project-detailview.html',
-  styleUrl: './project-detailview.scss',
-})
-export class ProjectDetailview {}
